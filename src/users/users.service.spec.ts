b0605143420f704a532e1241c628b948cedcf09e
Éricles Miller <ericlesmiller15@gmail.com>
2025-02-16 12:56:23 -0300

    feat: prepare test service user

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let repository: Repository<User>;
+
+  const user = new User(
+    100,
+    'test@test.com',
+    'John Doe',
+    '$2a$08$WBrratiQFGKpw8EySozjv.pq5JsNY2PcnnULRSwSNP/UOWBK9vkH6',
+  );
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOne: jest.fn(),
+            save: jest.fn(),
+            findAndCount: jest.fn(),
+            delete: jest.fn(),
+          },
+        },
+      ],
     }).compile();
-
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
     service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(repository).toBeDefined();
   });
 });
