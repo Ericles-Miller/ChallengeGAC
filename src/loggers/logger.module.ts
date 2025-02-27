93c7fbf0b7fe3cc358712ea6da0b0566fa642a5a
Éricles Miller <ericlesmiller15@gmail.com>
2025-02-27 17:47:30 -0300

    feat: create a new logs to transaction in els stack

--- src/loggers/logger.module.ts
+++ src/loggers/logger.module.ts
@@ -3,12 +3,13 @@ import { LoggerModule } from 'nestjs-pino';
 import { CustomLogger } from './custom-logger';
 import { LoggerMiddleware } from './logger-middleware';
 import 'dotenv/config';
+import { LoggerService } from './logger.service';
 
 @Module({
   imports: [LoggerModule.forRoot({ pinoHttp: { level: 'trace', autoLogging: false } })],
   controllers: [],
-  providers: [CustomLogger],
-  exports: [CustomLogger],
+  providers: [CustomLogger, LoggerService],
+  exports: [CustomLogger, LoggerService],
 })
 export class LoggersModule {
   configure(consumer: MiddlewareConsumer) {
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -6,9 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RevokedToken } from 'src/auth/entities/revoked-token.entity';
 import { Transaction } from './entities/transaction.entity';
 import { UsersModule } from 'src/users/users.module';
+import { LoggersModule } from 'src/loggers/logger.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, TypeOrmModule.forFeature([Transaction, RevokedToken])],
+  imports: [AuthModule, UsersModule, TypeOrmModule.forFeature([Transaction, RevokedToken]), LoggersModule],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
